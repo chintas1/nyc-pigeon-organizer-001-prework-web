d83a9fa57d271f132aaea94d4198b321cc3888ef
chintas1 <chintas1@tcnj.edu>
2015-12-26 22:07:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organized = {}
+  data.each do |info_type, info|
+    info.each do |attribute, pigeons|
+      pigeons.each do |pigeon|
+        if(!(organized.keys.include?(pigeon)))
+          organized[pigeon] = {color: [], gender: [], lives: []}
+          organized[pigeon][info_type] << attribute.to_s
+          puts(organized)
+        else
+          puts(organized)
+          organized[pigeon][info_type] << attribute.to_s
+        end
+      end
+    end
+  end
+  organized
 end
