e052ff96058ec86507c05640d4f7ee3e44908cf8
chintas1 <chintas1@tcnj.edu>
2016-02-04 09:09:35 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,36 +1,61 @@
 require 'pry'
+def get_pigeons_name_array(initial_data)
+  initial_data.fetch(:gender).values.flatten
+end
+
 def nyc_pigeon_organizer(initial_data)
-  # write your code here!
-  template = new_data_form_template(pigeons_name_array(initial_data))
-  template = fill_with_initial_data(initial_data, template, :gender)
-  template = fill_with_initial_data(initial_data, template, :color)
-  fill_with_initial_data(initial_data, template, :lives)
-end
-
-def fill_with_initial_data(initial_data, template, attribute)
-  template.keys.each do |pigeon|
-    template = get_pigeon_initial_data(initial_data, template, pigeon, attribute)
-  end
-  template
-end
-
-def get_pigeon_initial_data(initial_data, template, pigeon, attribute)
-  initial_data.fetch(attribute).each do |pigeon_attribute_value, pigeons_w_attribute|
-    template.fetch(pigeon).fetch(attribute) << pigeon_attribute_value.to_s if pigeons_w_attribute.include?(pigeon)
-  end
-  template
-end
-
-def pigeons_name_array(initial_data)
-  initial_data.fetch(:gender).fetch(:male) + initial_data.fetch(:gender).fetch(:female)
-end
-
-def new_data_form_template(pigeon_names)
-  pigeon_names.each_with_object({}) do |pigeon_name, new_data_template|
-    new_data_template[pigeon_name] = {color: [], gender: [], lives:[]}
+  get_pigeons_name_array(initial_data).each_with_object({}) do |pigeon_name, formatted_hash|
+    formatted_hash[pigeon_name] = get_pigeon_data_hash(initial_data, pigeon_name)
   end
 end
 
+def get_pigeon_data_hash(initial_data, pigeon_name)
+  initial_data.keys.each_with_object({}) do |attribute_type, pigeon_data_hash|
+    pigeon_data_hash[attribute_type] = get_attribute_array(initial_data, pigeon_name, attribute_type)
+  end
+end
+
+def get_attribute_array(initial_data, pigeon_name, attribute_symbol)
+  initial_data.fetch(attribute_symbol).collect do |attribute_value, pigeons_w_attribute|
+    attribute_value.to_s if pigeons_w_attribute.include?(pigeon_name)
+  end.compact
+end
+
+##### Old Version With Template
+# def nyc_pigeon_organizer(initial_data)
+#   # write your code here!
+#   template = new_data_form_template(pigeons_name_array(initial_data))
+#   template = fill_with_initial_data(initial_data, template, :gender)
+#   template = fill_with_initial_data(initial_data, template, :color)
+#   fill_with_initial_data(initial_data, template, :lives)
+# end
+
+# def fill_with_initial_data(initial_data, template, attribute)
+#   template.keys.each do |pigeon|
+#     template = get_pigeon_initial_data(initial_data, template, pigeon, attribute)
+#   end
+#   template
+# end
+
+# def get_pigeon_initial_data(initial_data, template, pigeon, attribute)
+#   initial_data.fetch(attribute).each do |pigeon_attribute_value, pigeons_w_attribute|
+#     template.fetch(pigeon).fetch(attribute) << pigeon_attribute_value.to_s if pigeons_w_attribute.include?(pigeon)
+#   end
+#   template
+# end
+
+# def pigeons_name_array(initial_data)
+#   initial_data.fetch(:gender).fetch(:male) + initial_data.fetch(:gender).fetch(:female)
+# end
+
+# def new_data_form_template(pigeon_names)
+#   pigeon_names.each_with_object({}) do |pigeon_name, new_data_template|
+#     new_data_template[pigeon_name] = {color: [], gender: [], lives:[]}
+#   end
+# end
+
+
+##### Old Version With Broken Up fill method
 # def fill_gender(initial_data, template)
 #   template.keys.each do |pigeon|
 #     template = get_pigeon_gender(initial_data, template, pigeon)
@@ -58,28 +83,3 @@ end
 #   end
 #   template
 # end
-
-
-
-
-
-nyc_pigeon_organizer({
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-})
-
-
